e9df92ce6cfcdf206396660ccc2954695ebb8f3f
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-15 12:54:12 +0300

    created ProductController

---
+++ product-service/src/main/java/com/hakan/microservices/product/webApi/controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.hakan.microservices.product.webApi.controllers;
+
+
+import com.hakan.microservices.product.entities.concretes.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/product")
+public class ProductController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+
+    }
+}
