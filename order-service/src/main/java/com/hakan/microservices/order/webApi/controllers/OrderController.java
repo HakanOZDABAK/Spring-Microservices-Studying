6d7cc7e97333c1833286029dc8cffbf649af1e28
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-16 14:05:12 +0300

    added OrderController and flyway migration

---
+++ order-service/src/main/java/com/hakan/microservices/order/webApi/controllers/OrderController.java
@@ -0,0 +1,23 @@
+package com.hakan.microservices.order.webApi.controllers;
+
+import com.hakan.microservices.order.business.abstracts.OrderService;
+import com.hakan.microservices.order.business.requests.OrderRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        this.orderService.placeOrder(orderRequest);
+        return "Order Place Successfully";
+    }
+
+}
