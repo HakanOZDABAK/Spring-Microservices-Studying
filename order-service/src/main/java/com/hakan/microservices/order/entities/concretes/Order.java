eeef6700bf1989e29c7c913ff61db6688c6caf2b
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-16 13:22:55 +0300

    created order entity

---
+++ order-service/src/main/java/com/hakan/microservices/order/entities/concretes/Order.java
@@ -0,0 +1,28 @@
+package com.hakan.microservices.order.entities.concretes;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_orders")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
