1e8fa924633803f024a3cddc08904608ec947717
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-16 13:34:15 +0300

    added placeOrder method

--- order-service/src/main/java/com/hakan/microservices/order/business/concretes/OrderManager.java
+++ order-service/src/main/java/com/hakan/microservices/order/business/concretes/OrderManager.java
@@ -1,7 +1,26 @@
 package com.hakan.microservices.order.business.concretes;
 
+import com.hakan.microservices.order.business.abstracts.OrderService;
+import com.hakan.microservices.order.business.requests.OrderRequest;
+import com.hakan.microservices.order.dataAccess.abstracts.OrderRepository;
+import com.hakan.microservices.order.entities.concretes.Order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
-public class OrderManager {
+@RequiredArgsConstructor
+public class OrderManager implements OrderService{
+
+    private final OrderRepository orderRepository;
+    @Override
+    public void placeOrder(OrderRequest orderRequest) {
+      Order order =  new Order();
+      order.setOrderNumber(UUID.randomUUID().toString());
+      order.setPrice(orderRequest.price());
+      order.setQuantity(orderRequest.quantity());
+      order.setSkuCode(orderRequest.skucCode());
+      this.orderRepository.save(order);
+    }
 }
