3b4427a7ef5987fa2f872bc1e83709f8a13a8ca7
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-17 11:25:31 +0300

    added logical get request to InventoryController

--- inventory-service/src/main/java/com/hakan/microservices/inventory/webApi/controllers/InventoryController.java
+++ inventory-service/src/main/java/com/hakan/microservices/inventory/webApi/controllers/InventoryController.java
@@ -1,4 +1,23 @@
 package com.hakan.microservices.inventory.webApi.controllers;
 
+
+import com.hakan.microservices.inventory.business.abstracts.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
 public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean isInStock(@RequestParam String skuCode,@RequestParam Integer quantity)
+    {
+        return this.inventoryService.isInStock(skuCode,quantity);
+
+    }
 }
