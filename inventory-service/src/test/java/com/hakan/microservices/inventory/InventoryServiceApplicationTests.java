b54b2ea485f7a9c591b77790fe3ad95b468f94c7
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-17 10:31:43 +0300

    InventoryServiceApplication is initialized

---
+++ inventory-service/src/test/java/com/hakan/microservices/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakan.microservices.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
