76c11a26f1e4993b9106a8ca6249a65295e1b666
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-18 10:37:55 +0300

    added inventoryClient method to OrderManager

--- order-service/src/main/java/com/hakan/microservices/order/business/concretes/OrderManager.java
+++ order-service/src/main/java/com/hakan/microservices/order/business/concretes/OrderManager.java
@@ -2,6 +2,7 @@ package com.hakan.microservices.order.business.concretes;
 
 import com.hakan.microservices.order.business.abstracts.OrderService;
 import com.hakan.microservices.order.business.requests.OrderRequest;
+import com.hakan.microservices.order.client.InventoryClient;
 import com.hakan.microservices.order.dataAccess.abstracts.OrderRepository;
 import com.hakan.microservices.order.entities.concretes.Order;
 import lombok.RequiredArgsConstructor;
@@ -14,13 +15,23 @@ import java.util.UUID;
 public class OrderManager implements OrderService{
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     @Override
     public void placeOrder(OrderRequest orderRequest) {
-      Order order =  new Order();
-      order.setOrderNumber(UUID.randomUUID().toString());
-      order.setPrice(orderRequest.price());
-      order.setQuantity(orderRequest.quantity());
-      order.setSkuCode(orderRequest.skuCode());
-      this.orderRepository.save(order);
+      var isProductInStock =   this.inventoryClient.isInStock(orderRequest.skuCode(),orderRequest.quantity());
+
+      if(isProductInStock){
+          Order order =  new Order();
+          order.setOrderNumber(UUID.randomUUID().toString());
+          order.setPrice(orderRequest.price());
+          order.setQuantity(orderRequest.quantity());
+          order.setSkuCode(orderRequest.skuCode());
+          this.orderRepository.save(order);
+      }
+      else {
+
+          throw new RuntimeException("Product with SkuCode" + orderRequest.skuCode()+ " is not in  stock");
+      }
+
     }
 }
