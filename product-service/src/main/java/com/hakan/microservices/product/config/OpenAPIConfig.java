c92992d541493049280091d182bac3c45e8e0fb7
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-29 13:48:21 +0300

    edited OpenAPIConfig.java

--- product-service/src/main/java/com/hakan/microservices/product/config/OpenAPIConfig.java
+++ product-service/src/main/java/com/hakan/microservices/product/config/OpenAPIConfig.java
@@ -1,6 +1,9 @@
 package com.hakan.microservices.product.config;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,6 +12,11 @@ public class OpenAPIConfig {
     @Bean
     public OpenAPI productServiceAPI(){
 
-        return new OpenAPI()
+        return new OpenAPI().info(new Info().title("Product Service API")
+                .description("For Product Service")
+                .version("v1")
+                .license(new License().name("Apache 2.0")))
+                .externalDocs(new ExternalDocumentation().description("You can refer to the Product Service Wiki Doc")
+                        .url("https://product-servşce-dummy-url.com/docs"));
     }
 }
