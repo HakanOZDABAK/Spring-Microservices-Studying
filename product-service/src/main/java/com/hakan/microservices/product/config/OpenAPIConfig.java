d9e4e0c7d96e81d7807f541bcd96cd8cd4728539
HakanOZDABAK <hakanozdabak2000@hotmail.com>
2024-04-29 12:38:32 +0300

    added OpenAPIConfig.java class to config springdoc

---
+++ product-service/src/main/java/com/hakan/microservices/product/config/OpenAPIConfig.java
@@ -0,0 +1,14 @@
+package com.hakan.microservices.product.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+    @Bean
+    public OpenAPI productServiceAPI(){
+
+        return new OpenAPI()
+    }
+}
